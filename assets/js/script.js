d18aaf809d63ab20355ea728ec8546e39f54e045
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-01-22 12:55:04 +1100

    added current weather in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,72 @@
 // Declared Variables
 var currentDayEl = $('#currentDay');
-var searchFormEl = document.querySelector('#search-form');
+var searchFormEl = document.querySelector('#search-section');
+var searchButton = document.querySelector('#searchButton');
+
 
 // Generated apiKey from openweatherAPI.com after creating an account
-var apiWeatherKey = 'cbb0bb358215c29546dabe7a6b463f5f';
+var baseURL = 'https://api.openweathermap.org';
+var apiWeatherKey = 'eca2fe1873a9d22d6e99bc36262d9d3f';
 
-// To get current weather:
-var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiWeatherKey}`;
 
-// To get weather icon:
-var iconCode = response.weather[0].icon;
-var weatherIconURL = `https://openweatherapp.org/img/w/${iconCode}.png`;
 
-// To get UV:
-var uvURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiWeatherKey}`;
+function weatherToday(city) {
+    var weatherTodayURL = `${baseURL}/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  
+    fetch(weatherTodayURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            $("#currentWeatherDetails").css("display", "block");
+            $("#cityInformation").empty();
+  
+            var weatherIconID = data.weather[0].icon;
+            var weatherIconImageURL = `${baseURL}/img/w/${weatherIconID}.png`;
 
-// To get the forecast for next 5 days:
-var futureWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiWeatherKey}`;
+            var weatherCityCurrent = $(`
+                <h2 id="weatherCityCurrent">
+                ${data.name} ${today} <img src="${weatherIconImageURL}" alt="${data.weather[0].description}" />
+                </h2>
+                <p>Temperature: ${data.main.temp} °C</p>
+                <p>Humidity: ${data.main.humidity}\%</p>
+                <p>Wind Speed: ${data.wind.speed} MPH</p>
+            `);
+            $("#cityInformation").append(weatherCityCurrent);
+
+            // Run the Weather Forecast
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+
+            futureWeather(lat, lon);
+
+        })
+        //   Display error message to the user otherwise
+        .catch(error => {
+            console.error('Error fetching current weather: ', error);
+            alert('Error fetching current weather data.');
+        });
+
+
+  }
+
+
+
+
+// function forecastNext5Days(lat,lon) {
+//     var forecastWeatherURL = `${baseURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiWeatherKey}`;
+//     fetch(forecastWeatherURL)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       return displayForecast(data);
+//     }) 
+//     .catch(function(error) {
+//         console.error('Error fetching forecast weather: ', error);
+//         alert('Error fetching forecast weather data.');
+//     });   
+// };
+
+
+
 
