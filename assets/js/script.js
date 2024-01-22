17b8d1331da3aef5d1d5521c38bc1d16f4f9da86
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-01-22 14:05:38 +1100

    updating javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,13 @@ var searchButton = document.querySelector('#searchButton');
 
 // Generated apiKey from openweatherAPI.com after creating an account
 var baseURL = 'https://api.openweathermap.org';
+var metric = `units=metric`;
 var apiWeatherKey = 'eca2fe1873a9d22d6e99bc36262d9d3f';
 
 
 
 function weatherToday(city) {
-    var weatherTodayURL = `${baseURL}/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    var weatherTodayURL = `${baseURL}/data/2.5/weather?q=tokyo&${metric}&appid=${apiWeatherKey}`;
   
     fetch(weatherTodayURL)
         .then(response => response.json())
@@ -28,16 +29,17 @@ function weatherToday(city) {
                 ${data.name} ${today} <img src="${weatherIconImageURL}" alt="${data.weather[0].description}" />
                 </h2>
                 <p>Temperature: ${data.main.temp} °C</p>
+                <p>Wind: ${data.wind.speed} MPH</p>
                 <p>Humidity: ${data.main.humidity}\%</p>
-                <p>Wind Speed: ${data.wind.speed} MPH</p>
+                
             `);
             $("#cityInformation").append(weatherCityCurrent);
 
             // Run the Weather Forecast
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
+            var latitude = data.coord.lat;
+            var longitude = data.coord.lon;
 
-            futureWeather(lat, lon);
+            forecastNext5Days(latitude, longitude);
 
         })
         //   Display error message to the user otherwise
@@ -45,28 +47,25 @@ function weatherToday(city) {
             console.error('Error fetching current weather: ', error);
             alert('Error fetching current weather data.');
         });
+}
 
 
-  }
+function forecastNext5Days(latitude,longitude) {
+    var forecastWeatherURL = `${baseURL}/data/2.5/onecall?lat=${latitude}&lon=${longitude}&${metric}&appid=${apiWeatherKey}`;
+    
+    fetch(forecastWeatherURL)
+        .then(response => response.json())
+        .then(dataFutureWeather => {
+            console.log(dataFutureWeather);
+            $("#forecastInformation").empty();
+        })
 
 
-
-
-// function forecastNext5Days(lat,lon) {
-//     var forecastWeatherURL = `${baseURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiWeatherKey}`;
-//     fetch(forecastWeatherURL)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       return displayForecast(data);
-//     }) 
-//     .catch(function(error) {
-//         console.error('Error fetching forecast weather: ', error);
-//         alert('Error fetching forecast weather data.');
-//     });   
-// };
-
-
+        //   Display error message to the user otherwise
+        .catch(error => {
+            console.error('Error fetching future weather: ', error);
+            alert('Error fetching future weather data.');
+        });
+}
 
 
