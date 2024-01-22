bb817428d4159d46fdd27f33f7aa833fd6615f06
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-01-23 02:23:29 +1100

    added future weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,7 @@
-// Declared Variablescd 
-var searchFormEl = document.querySelector('#search-section');
-var searchButton = document.querySelector('#searchButton');
-
-
 // Generated apiKey from openweatherAPI.com after creating an account
 var baseURL = 'https://api.openweathermap.org';
 var metric = `units=metric`;
-var apiWeatherKey = 'eca2fe1873a9d22d6e99bc36262d9d3f';
+var apiWeatherKey = '24716f334fbb25fbc76f035aa23a8fa8';
 
 
 // Function to fetch and display current weather for a given city
@@ -34,9 +29,9 @@ function weatherToday(city) {
 
             // HTML content for city's current weather information
             var weatherCityCurrent = $(`
-                <h2 id="weatherCityCurrent">
+                <h3 id="weatherCityCurrent">
                 ${data.name} ` + dayjs().format(`DD/MM/YYYY`) + ` <img src="${weatherIconImageURL}" alt="${data.weather[0].description}" />
-                </h2>
+                </h3>
                 <p>Temperature: ${data.main.temp} °C</p>
                 <p>Wind: ${data.wind.speed} MPH</p>
                 <p>Humidity: ${data.main.humidity}\%</p>
@@ -50,7 +45,7 @@ function weatherToday(city) {
             var longitude = data.coord.lon;
 
             // running the weather forecast function that has been created later down the javascript code
-            forecastNext5Days(latitude, longitude);
+            forecastNext4Days(latitude, longitude);
 
         })
         // Display error message to the user otherwise
@@ -61,22 +56,59 @@ function weatherToday(city) {
 }
 
 
-function forecastNext5Days(latitude,longitude) {
-    var forecastWeatherURL = `${baseURL}/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiWeatherKey}&${metric}`;
+function forecastNext4Days(latitude,longitude) {
+    var forecastWeatherURL = `${baseURL}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiWeatherKey}&${metric}`;
     
     fetch(forecastWeatherURL)
         .then(response => response.json())
         .then(dataFutureWeather => {
             console.log(dataFutureWeather);
             $("#forecastInformation").empty();
+            // i = 1 shows todays weather. Want to see from tomorrows weather.
+            for (let i = 2; i <= 5; i++) {
+                // check api. Theres only up to i = 39 without *8.
+                // Multiply 8 because dt_txt goes every 3 hours and there are 24 hours in a day.
+                var index = i*8;
+                var cityWeatherInformation = {
+                    date: dataFutureWeather.list[index-1].dt_txt,
+                    humidity: dataFutureWeather.list[index-1].main.humidity,
+                    icon: dataFutureWeather.list[index-1].weather[0].icon,
+                    temperature: dataFutureWeather.list[index-1].main.temp,
+                    wind: dataFutureWeather.list[index-1].wind.speed
+                };
+                
+                // Logging on console
+                console.log(cityWeatherInformation);
+
+                var IconForWeatherURL = `<img src="${baseURL}/img/w/${cityWeatherInformation.icon}.png" alt="${dataFutureWeather.list[index-1].weather[0].main}" />`;
+                var currentDate = dayjs(cityWeatherInformation.date).format('DD/MM/YYYY');
+
+        
+                // Display weather forecast for the next five days
+                // Future weather information includes weather icon, temperature, wind speed, and humidity
+                var futureWeatherCard = $(`
+                    <div class="pl-3">
+                        <div class="card pl-3 pt-3 mb-3 bg" style="width: 12rem;>
+                            <div class="card-body">
+                                <h5>${currentDate}</h5>
+                                <p>${IconForWeatherURL}</p>
+                                <p>Temperature: ${cityWeatherInformation.temperature} °C</p>
+                                <p> Wind: ${cityWeatherInformation.wind} m/s</p>
+                                <p>Humidity: ${cityWeatherInformation.humidity}\%</p>
+                            </div>
+                        </div>
+                    <div>
+`);
+                $("#forecastInformation").append(futureWeatherCard);
+            }
         })
 
-
-        //   Display error message to the user otherwise
+        // Display error message to the user otherwise
         .catch(error => {
             console.error('Error fetching future weather: ', error);
             alert('Error fetching future weather data.');
         });
 }
+
 
 
