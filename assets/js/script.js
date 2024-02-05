111c9a1108c6c9a59b42373cb823766e20d198e9
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-05 12:15:28 +1100

    updated search history list to 10 from 13

--- assets/js/script.js
+++ assets/js/script.js
@@ -171,8 +171,8 @@ function saveSearchedCity(city){
         // Add to the beginning with capitalisation
         searchHistoryList.unshift(city); 
 
-        if (searchHistoryList.length > 13) {
-            // Remove the oldest entry if the list exceeds 13
+        if (searchHistoryList.length > 10) {
+            // Remove the oldest entry if the list exceeds 10
             searchHistoryList.pop(); 
         }
         // Save the updated history to localStorage
